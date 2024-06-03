fbaff8aefde0313570ccce1e855e6a3b75810631
colinnnnnnnnnnn <kalinpoiata@gmail.com>
2024-06-03 16:25:57 +0300

    Add grid layout

--- src/home.js
+++ src/home.js
@@ -9,6 +9,19 @@ export function generateHome() {
     content.appendChild(slogan);
 
     const reviews = document.createElement('ul');
-    const review1 = document.createElement('li')
+    const review1 = document.createElement('li');
+    review1.innerText = 'Wow! This is truly a thing!';
+    reviews.appendChild(review1);
+
+    const review2 = document.createElement('li');
+    review2.innerText = 'I have finally discovered the feeling of nothing! A marvel of mediocrity!';
+    reviews.appendChild(review2);
+
+    const review3 = document.createElement('li');
+    review3.innerText = 'I have never been so whelmed in my entire life!';
+    reviews.appendChild(review3);
+
+    content.appendChild(reviews);
+    
 
 }
