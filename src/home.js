51e9c51820ce98eac4ec5666ab25f75ddb190019
colinnnnnnnnnnn <kalinpoiata@gmail.com>
2024-06-03 16:56:15 +0300

    Space content

--- src/home.js
+++ src/home.js
@@ -5,10 +5,17 @@ export function generateHome() {
     content.innerHTML = '';
 
     const slogan = document.createElement('div');
-    slogan.innerText = '"Overpriced mediocre food - this is Mamaliga Bar"';
+    slogan.innerText = 'Overpriced mediocre food - this is Mamaliga Bar';
+    slogan.classList.add('slogan');
     content.appendChild(slogan);
 
+    const subtext = document.createElement('div');
+    subtext.innerText = "Don't believe us? Here are some of our best reviews:"
+    content.appendChild(subtext);
+
     const reviews = document.createElement('ul');
+    reviews.classList.add('reviews');
+
     const review1 = document.createElement('li');
     review1.innerText = 'Wow! This is truly a thing!';
     reviews.appendChild(review1);
@@ -18,7 +25,7 @@ export function generateHome() {
     reviews.appendChild(review2);
 
     const review3 = document.createElement('li');
-    review3.innerText = 'I have never been so whelmed in my entire life!';
+    review3.innerText = 'I have never been so whelmed in my entire life! The definition of 3/5 stars!';
     reviews.appendChild(review3);
 
     content.appendChild(reviews);
